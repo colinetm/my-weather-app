a7bfcea48e7499609b99223ed982736b29bbfc28
colinetm <colinetm@gmail.com>
2026-03-16 16:32:24 +0100

    security: passage à une variable d'environnement pour l'API Key

--- app/api/weather.ts
+++ app/api/weather.ts
@@ -1,12 +1,23 @@
-import axios from 'axios'
+import axios from 'axios';
 
+export default async function weather() {
+    // On récupère la clé depuis les variables d'environnement
+    const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 
-
-export default async function weather(){
     try {
-        const response = await axios.get('https://api.openweathermap.org/data/3.0/onecall?lat=48.893217&lon=2.287864&exclude=current,hourly,minutely,alerts&lang=fr&units=metric&appid=ApiKey')
-        return response
+        const response = await axios.get('https://api.openweathermap.org/data/3.0/onecall', {
+            params: {
+                lat: 48.893217,
+                lon: 2.287864,
+                exclude: 'current,hourly,minutely,alerts',
+                lang: 'fr',
+                units: 'metric',
+                appid: apiKey // Utilisation de la variable
+            }
+        });
+        return response;
     } catch (error) {
-        console.log(error)
+        console.error("Erreur lors de l'appel API :", error);
+        throw error; // Il est souvent préférable de renvoyer l'erreur pour la gérer dans l'UI
     }
 }
